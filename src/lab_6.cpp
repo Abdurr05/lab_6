984e45cc25224199f52d126cceff02bf8ab42f45
Abdurr05 <abdurr2005@gmail.com>
2021-07-02 14:36:58 +0300

    first commit

---
+++ src/lab_6.cpp
@@ -0,0 +1,45 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "c:/Users/Abdurrahman/Documents/labs/lab_6/src/lab_6.ino"
+#include "oled-wing-adafruit.h"
+
+void setup();
+void loop();
+#line 3 "c:/Users/Abdurrahman/Documents/labs/lab_6/src/lab_6.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  display.setup();
+
+	display.clearDisplay();
+	display.display();
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  display.loop();
+  //The formula to get celsius from the TMP36
+  uint64_t reading = analogRead(A5);
+  double voltage = (reading * 3.3) / 4095.0;
+  double temperature = (voltage - 0.5) * 100;
+  // to display the temperature
+  if (display.pressedA()) {
+    display.clearDisplay();
+
+		display.setTextSize(1);
+		display.setTextColor(WHITE);
+		display.setCursor(0,0);
+    display.println("your temperature is ");
+		display.println(temperature);
+		display.display();
+  }
+  // The core of your code will likely live here.
+
+}
---
+++ src/lab_6.ino
@@ -0,0 +1,36 @@
+#include "oled-wing-adafruit.h"
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+OledWingAdafruit display;
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+  display.setup();
+
+	display.clearDisplay();
+	display.display();
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  display.loop();
+  //The formula to get the temperature in celsius from the TMP36
+  uint64_t reading = analogRead(A5);
+  double voltage = (reading * 3.3) / 4095.0;
+  double temperature = (voltage - 0.5) * 100;
+  // to display the temperature
+  if (display.pressedA()) {
+    display.clearDisplay();
+
+		display.setTextSize(1);
+		display.setTextColor(WHITE);
+		display.setCursor(0,0);
+    display.println("your temperature is ");
+		display.println(temperature);
+		display.display();
+  }
+  // The core of your code will likely live here.
+
+}
